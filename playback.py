a95746ddd744d07644892d188a97c34a54dec454
Kenoyugi5 <koyugi02@gmail.com>
2023-04-05 20:56:34 +0300

    Assignment 1

--- playback.py
+++ playback.py
@@ -1,5 +1,6 @@
 idea = input("Enter text:")
 
-#Inserts ... after every letter
+# `loading = idea.replace("","...")` is replacing every empty space in the `idea` string with three
+# dots (`...`) and assigning the resulting string to the variable `loading`.
 loading = idea.replace("","...")
 print(loading)
