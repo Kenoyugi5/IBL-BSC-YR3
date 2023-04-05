43c92816129744201e03594c93fdedd9da6b23f8
Kenoyugi5 <koyugi02@gmail.com>
2023-04-05 14:54:24 +0300

    Prints in lowercase

--- indoor.py
+++ indoor.py
@@ -1,2 +1,4 @@
 colour = input("What is your favorite colour:")
+
+#Prints the input in lowercase
 print("Favorite colour is: {}".format(colour).lower())
---
+++ wed.py
@@ -0,0 +1,7 @@
+name = input("What is your name?")
+
+#Removes space in the terminal
+name = name.strip()
+
+#Prints Hello + the input name
+print("Hello", name)
