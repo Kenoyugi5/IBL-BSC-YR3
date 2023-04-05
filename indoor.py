dd72384275868c8b19502eb97b801fcb724716aa
Kenoyugi5 <koyugi02@gmail.com>
2023-04-05 20:54:43 +0300

    Assignment 1

--- indoor.py
+++ indoor.py
@@ -1,4 +1,7 @@
 colour = input("What is your favorite colour:")
 
-#Prints the input in lowercase
+
+# This line of code is printing a string that includes the user's favorite color in lowercase letters.
+# The `format()` method is used to insert the value of the `colour` variable into the string, and the
+# `lower()` method is used to convert the color to lowercase letters.
 print("Favorite colour is: {}".format(colour).lower())
